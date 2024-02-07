8f35ee093c51b173f1aedf5b0ee58e6cd715166c
MR-SAJAN-SAH <22054081@kiit.ac.in>
2024-02-07 23:48:22 +0530

    commit 2

--- OS LAB/lab3/q10.sh
+++ OS LAB/lab3/q10.sh
@@ -1,14 +1,23 @@
 # Write a shell program to calculate area of triangle, rectangle, and circle.
 
-echo "Enter the side of the square:"
-read s
-echo "Enter the length and breadth of rectangle:"
-read leng
-read brea
+echo "Enter the breadth and height of the triangle:"
+read tb
+read th
+t=$(echo "$tb * $th" | bc)
+area_triangle=$(echo "scale=2; $t / 2" | bc)
+
+echo "Enter the length and breadth of the rectangle:"
+read rl
+read rb
+area_rectangle=$(echo "scale=2; $rl * $rb" | bc)
+
 echo "Enter the radius of the circle:"
 read radius
-echo "Area of the square is:` expr $s \* $s ` "
-echo "Area of the rectangle is: `expr $leng \* $brea`"
-echo "Area of the circle is: `expr 3.14 \* radius \* radius`"
+c=3.14
+area_circle=$(echo "scale=2; $c * $radius * $radius" | bc)
+
+echo "Area of the triangle is: $area_triangle"
+echo "Area of the rectangle is: $area_rectangle"
+echo "Area of the circle is: $area_circle"
 
 read -p "Press Enter to exit"
--- OS LAB/lab3/q11.sh
+++ OS LAB/lab3/q11.sh
@@ -9,13 +9,13 @@ if [ $choice -eq 1 ]
 then
 	echo -n "Enter temperature (C) : "
 	read tc
-	tf=$(echo "scale=2;((9/5) * $tc) + 32" |bc)
+	tf=$((echo "scale=2;((9/5) * $tc) + 32" ))
 	echo "$tc C = $tf F"
 elif [ $choice -eq 2 ]
 then
 	echo -n "Enter temperature (F) : "
 	read tf
-	tc=$(echo "scale=2;(5/9)*($tf-32)"|bc)
+	tc=$((echo "scale=2;(5/9)*($tf-32)"))
 	echo "$tf = $tc"
 else
 	echo "Please select 1 or 2 only"
